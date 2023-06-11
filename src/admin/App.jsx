1f3ebcc68bd64b157f4551b65a256bec6e1932b9
cassiopia <cassiopia@ya.ru>
2023-06-11 17:35:35 +0300

    get posts from db

--- src/admin/App.jsx
+++ src/admin/App.jsx
@@ -1,40 +1,15 @@
 import * as React from "react";
-import {Admin, Resource, fetchUtils} from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
-import simpleRestProvider from 'ra-data-simple-rest';
+import {Admin, Resource} from 'react-admin';
 import postgrestRestProvider from '@raphiniert/ra-data-postgrest';
 import authProvider from './authProvider';
 import {Login, Layout} from './layout';
 import {greenTheme} from './layout/themes';
-import {TestCommentsList, TestPostList, TestTmpList} from "./TestList";
 import {PortfolioList} from "./PortfolioList";
 import {PostList} from "./PostList";
 
-//console.log('pageDataProvider', pageDataProvider.getOne());
+const dataPostgreeProvider = postgrestRestProvider('http://localhost:3001/posts');
 
-const httpClient = (url, options = {}) => {
-    if (!options.headers) {
-        options.headers = new Headers({ Accept: 'application/json' });
-    }
-    // add your own headers here
-    options.headers.set('X-Custom-Header', 'foobar');
-    return fetchUtils.fetchJson(url, options);
-};
-
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-//const dataSimpleRestProvider = simpleRestProvider('http://localhost:3010/portfolio/get_albums');
-//const dataPostgreeProvider = postgrestRestProvider('http://localhost:3010/portfolio');
-const dataPostgreeProvider = postgrestRestProvider('http://localhost:3001/experiments');
-
-//console.log('dataProvider @@@@@@@ ', dataProvider);
 console.log('dataPostgreeProvider', dataPostgreeProvider);
-
-// TODO нужно сделать выборку с этого адреса
-//http://localhost:3010/portfolio/get_albums
-
-
-// todo на сервере запрашивается такорй путь http://localhost:3001/experiments/experiments?order=id.asc&offset=0&limit=10, придумать как с этим быть
-
 
 const App = () => (
     <>
